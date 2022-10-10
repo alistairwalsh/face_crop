f21889a5a06b1281c87dfd2fee18a8d6e3252d46
Alistair Walsh <alistair.walsh@me.com>
2022-10-10 11:02:04 +1100

    attempt to not save file

--- pages/Face_cropping.py
+++ pages/Face_cropping.py
@@ -6,7 +6,7 @@ mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
 # For static images:
-IMAGE_FILES = ['face_1.jpg','face_2.jpg','face_3.jpg','face_4.jpg']
+IMAGE_FILES = ['face_1.jpg']
 with mp_face_detection.FaceDetection(
     model_selection=1, min_detection_confidence=0.5) as face_detection:
   for idx, file in enumerate(IMAGE_FILES):
@@ -23,12 +23,13 @@ with mp_face_detection.FaceDetection(
       print(mp_face_detection.get_key_point(
           detection, mp_face_detection.FaceKeyPoint.NOSE_TIP))
       mp_drawing.draw_detection(annotated_image, detection)
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+    #cv2.imwrite('annotated_image' + str(idx) + '.png', annotated_image)
+    image_1 = annotated_image
 
-image_1 = Image.open('/tmp/annotated_image0.png')
-image_2 = Image.open('/tmp/annotated_image1.png')
-image_3 = Image.open('/tmp/annotated_image2.png')
-image_4 = Image.open('/tmp/annotated_image3.png')
+#image_1 = Image.open('annotated_image0.png')
+#image_2 = Image.open('annotated_image1.png')
+#image_3 = Image.open('annotated_image2.png')
+#image_4 = Image.open('annotated_image3.png')
 
 col1, col2, col3 = st.columns([1,6,1])
 
@@ -38,9 +39,9 @@ with col1:
 with col2:
     st.title("Face cropping")
     st.image(image_1)
-    st.image(image_2)
-    st.image(image_3)
-    st.image(image_4)
+    #st.image(image_2)
+    #st.image(image_3)
+    #st.image(image_4)
 
 with col3:
     st.write("")
